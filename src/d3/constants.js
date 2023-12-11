36721221b2679553a3426a894a5732fc31027dd7
Tom Rees-Herdman <tom@techanvil.co.uk>
2023-12-11 16:15:27 +0000

    Show simple issues; pastelise colours.

---
+++ src/d3/constants.js
@@ -0,0 +1,19 @@
+export const rectWidth = 60;
+export const rectHeight = 35;
+
+export const pipelineAbbreviations = {
+  Triage: "T",
+  Stalled: "S",
+  Backlog: "B",
+  "Acceptance Criteria": "AC",
+  "Acceptance Criteria Review": "ACR",
+  "Implementation Brief": "IB",
+  "Implementation Brief Review": "IBR",
+  "Execution Backlog": "EB",
+  Execution: "E",
+  "Code Review": "CR",
+  "Merge Review": "MR",
+  QA: "QA",
+  Approval: "A",
+  Closed: "C",
+};
--- src/d3/index.js
+++ src/d3/index.js
@@ -4,38 +4,46 @@
 import * as d3 from "d3";
 import { dagStratify, sugiyama, decrossOpt } from "d3-dag";
 
-const pipelineColors = {
-  Triage: "#de425b",
-  Stalled: "#ea6158",
-  Backlog: "#f27e59",
-  "Acceptance Criteria": "#f79960",
-  "Acceptance Criteria Review": "#fbb46c",
-  "Implementation Brief": "#fdce7f",
-  "Implementation Brief Review": "#ffe796",
-  "Execution Backlog": "#e3d881",
-  Execution: "#c7c96d",
-  "Code Review": "#a9ba5b",
-  "Merge Review": "#8bac4b",
-  QA: "#6b9e3d",
-  Approval: "#488f31",
-  Closed: "#388f21",
-};
+/**
+ * Internal dependencies
+ */
+import { rectWidth, rectHeight } from "./constants";
+import { renderDetailedIssues } from "./detailed-issues";
+import { renderSimpleIssues } from "./simple-issues";
 
-const pipelineAbbreviations = {
-  Triage: "T",
-  Stalled: "S",
-  Backlog: "B",
-  "Acceptance Criteria": "AC",
-  "Acceptance Criteria Review": "ACR",
-  "Implementation Brief": "IB",
-  "Implementation Brief Review": "IBR",
-  "Execution Backlog": "EB",
-  Execution: "E",
-  "Code Review": "CR",
-  "Merge Review": "MR",
-  QA: "QA",
-  Approval: "A",
-  Closed: "C",
+// const pipelineColors = {
+//   Triage: "#de425b",
+//   Stalled: "#ea6158",
+//   Backlog: "#f27e59",
+//   "Acceptance Criteria": "#f79960",
+//   "Acceptance Criteria Review": "#fbb46c",
+//   "Implementation Brief": "#fdce7f",
+//   "Implementation Brief Review": "#ffe796",
+//   "Execution Backlog": "#e3d881",
+//   Execution: "#c7c96d",
+//   "Code Review": "#a9ba5b",
+//   "Merge Review": "#8bac4b",
+//   QA: "#6b9e3d",
+//   Approval: "#488f31",
+//   Closed: "#388f21",
+// };
+
+// Pastelised colors:
+const pipelineColors = {
+  Triage: "#f1c1cf",
+  Stalled: "#f4c3c2",
+  Backlog: "#f8d0c1",
+  "Acceptance Criteria": "#fbd9c7",
+  "Acceptance Criteria Review": "#fddfb3",
+  "Implementation Brief": "#ffe8c8",
+  "Implementation Brief Review": "#fff4db",
+  "Execution Backlog": "#f5efd3",
+  Execution: "#e4e7cc",
+  "Code Review": "#cdddb5",
+  "Merge Review": "#b9cfa7",
+  QA: "#a1ca9f",
+  Approval: "#89b85f",
+  Closed: "#7bb255",
 };
 
 function getIntersection(dx, dy, cx, cy, w, h) {
@@ -98,7 +106,7 @@ const panZoom = {
 export const generateGraph = (
   graphData,
   svgElement,
-  { showAncestorDependencies }
+  { showAncestorDependencies, showIssueDetails = false }
 ) => {
   try {
     panZoom.instance?.destroy();
@@ -118,8 +126,6 @@ export const generateGraph = (
   }
 
   const dag = dagStratify()(graphData);
-  const rectWidth = 60;
-  const rectHeight = 35;
   const nodeWidth = rectWidth * 1.5;
   const nodeHeight = rectHeight * 2;
   const arrowSize = nodeHeight / 2.0;
@@ -136,62 +142,6 @@ export const generateGraph = (
   ); // set node size instead of constraining to fit
   const { width, height } = layout(dag);
 
-  const padding = 3;
-
-  function truncate() {
-    const self = d3.select(this);
-
-    let textLength = self.node().getComputedTextLength(),
-      text = self.text();
-
-    while (textLength > rectWidth - 2 * padding && text.length > 0) {
-      text = text.slice(0, -1);
-      self.text(text + "\u2026");
-      textLength = self.node().getComputedTextLength();
-    }
-  }
-
-  function wrapLines(text, width, maxLines) {
-    text.each(function () {
-      let text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.1, // ems
-        x = text.attr("x"),
-        y = text.attr("y"),
-        dy = 0, //parseFloat(text.attr("dy")),
-        tspan = text
-          .text(null)
-          .append("tspan")
-          .attr("x", x)
-          .attr("y", y)
-          .attr("dy", dy + "em"),
-        lineCount = 1;
-
-      while ((word = words.pop())) {
-        line.push(word);
-        tspan.text(line.join(" "));
-        if (tspan.node().getComputedTextLength() > width) {
-          if (lineCount === maxLines) {
-            truncate.call(tspan.node());
-          } else {
-            lineCount++;
-            line.pop();
-            tspan.text(line.join(" "));
-            line = [word];
-            tspan = text
-              .append("tspan")
-              .attr("x", x)
-              .attr("y", y)
-              .attr("dy", ++lineNumber * lineHeight + dy + "em")
-              .text(word);
-          }
-        }
-      }
-    });
-  }
   const svgSelection = d3.select(svgElement);
   svgSelection.attr("viewBox", [0, 0, width, height].join(" "));
   const defs = svgSelection.append("defs"); // For gradients
@@ -205,16 +155,6 @@ export const generateGraph = (
 
   function getNodeColor(node) {
     return pipelineColors[node.data.pipelineName] || colorMap.get(node.data.id);
-  }
-
-  function getPipelineAbbreviation(node) {
-    return (
-      pipelineAbbreviations[node.data.pipelineName] ||
-      node.data.pipelineName
-        .match(/\b([A-Za-z0-9])/g)
-        .join("")
-        .toUpperCase()
-    );
   }
 
   // How to draw edges
@@ -338,77 +278,11 @@ export const generateGraph = (
     })
     .attr("fill", ({ target }) => getNodeColor(target));
 
-  // Add issue titles to nodes
-  nodes
-    .append("a")
-    .attr("href", (d) => d.data.htmlUrl)
-    .append("text")
-    .text((d) => d.data.title)
-    .attr("x", -rectWidth / 2 + padding)
-    .attr("y", -rectHeight / 2 + 6)
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 5)
-    .attr("text-anchor", "start")
-    .attr("alignment-baseline", "middle")
-    .attr("fill", "black")
-    .call(wrapLines, rectWidth - padding * 2, 3);
-
-  // Add assignees to nodes
-  nodes
-    // .append("a")
-    // .attr("href", (d) => d.data.htmlUrl)
-    .append("text")
-    .text((d) => d.data.assignees.join(", "))
-    .attr("x", -rectWidth / 2 + padding)
-    // .attr("y", -rectHeight / 2 + 6)
-    .attr("y", 5)
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 4)
-    .attr("text-anchor", "start")
-    .attr("alignment-baseline", "middle")
-    .attr("fill", "black")
-    .each(truncate);
-
-  // Add issue number to nodes
-  nodes
-    .append("a")
-    .attr("href", (d) => d.data.htmlUrl)
-    .append("text")
-    .text((d) => d.data.id)
-    .attr("x", -rectWidth / 2 + padding)
-    .attr("y", rectHeight / 2 - 5)
-    .attr("font-weight", "bold")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 5)
-    .attr("text-anchor", "start")
-    .attr("alignment-baseline", "middle")
-    .attr("fill", "black");
-
-  // Add pipeline name to nodes
-  nodes
-    .append("text")
-    .text((d) => getPipelineAbbreviation(d))
-    .attr("x", rectWidth / 2 - padding)
-    .attr("y", rectHeight / 2 - 5)
-    .attr("font-weight", "bold")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 5)
-    .attr("text-anchor", "end")
-    .attr("alignment-baseline", "middle")
-    .attr("fill", "black");
-
-  // Add "External" text for non-epic issues to nodes
-  nodes
-    .filter((d) => d.data.isNonEpicIssue)
-    .append("text")
-    .text("External")
-    .attr("y", rectHeight / 2 - 5)
-    .attr("font-weight", "bold")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 5)
-    .attr("text-anchor", "middle")
-    .attr("alignment-baseline", "middle")
-    .attr("fill", "black");
+  if (showIssueDetails) {
+    renderDetailedIssues(nodes);
+  } else {
+    renderSimpleIssues(nodes);
+  }
 
   // eslint-disable-next-line no-undef
   panZoom.instance = svgPanZoom("#zdg-graph", {
