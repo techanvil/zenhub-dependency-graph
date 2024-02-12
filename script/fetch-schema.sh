e2f354cc032cd91b107f45cc35cca5242af7e237
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-02-12 16:12:11 +0000

    Fix fetch-schema script (read API key from stdin).

--- script/fetch-schema.sh
+++ script/fetch-schema.sh
@@ -2,6 +2,12 @@
 
 repo_root=$( cd "$( dirname "$0}" )"/.. && pwd )
 
-zenhub_api_key=$( grep zenhubApiKey "$repo_root/src/config.json" | cut -d ' ' -f 4 | tr -d \" )
+echo Enter Zenhub API Key:
+
+read -r zenhub_api_key
+
+echo Fetching...
+
+# zenhub_api_key=$( grep zenhubApiKey "$repo_root/src/config.json" | cut -d ' ' -f 4 | tr -d \" )
 
 npx get-graphql-schema --header "Authorization=Bearer $zenhub_api_key" https://api.zenhub.com/public/graphql/ > "$repo_root/schema.graphql"
