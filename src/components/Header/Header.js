1ed3c0fb1ddf739006055c7087a54fe41f5728dc
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-06-14 15:13:50 +0100

    Remove redundant text.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -357,7 +357,6 @@ export default function Header({
                     <Text color="tomato">
                       <b>{closedIssues.length}</b> closed{" "}
                       {pluralise(closedIssues.length, "issue", "issues")} hidden
-                      (<b>{getOpenIssueCount(closedIssues)}</b> open)
                     </Text>
                   )}
                 </VStack>
