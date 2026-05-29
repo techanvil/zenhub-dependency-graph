a489c40ff7d671ec9ff92c5db83efcf7f0c6ad38
Tom Rees-Herdman <tom@techanvil.co.uk>
2026-05-29 11:50:00 +0100

    Initial commit.

--- src/components/SettingsModal/SettingsModal.jsx
+++ src/components/SettingsModal/SettingsModal.jsx
@@ -8,6 +8,7 @@ import {
   FormControl,
   FormHelperText,
   HStack,
+  Heading,
   Link,
   Input,
   Modal,
@@ -21,6 +22,11 @@ import {
   Radio,
   RadioGroup,
   Switch,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
 } from "@chakra-ui/react";
 import { deepEquals } from "../../utils/common";
 import {
@@ -38,6 +44,26 @@ import {
   pipelineHiddenAtom,
 } from "../../store/atoms";
 import { clearGraphCache } from "../../data/graph-data";
+
+function SwitchSetting({ label, settingKey, settings, onChange }) {
+  return (
+    <FormControl pt="4">
+      <FormLabel mb="1">{label}</FormLabel>
+      <Switch
+        isChecked={settings[settingKey]}
+        onChange={(e) => onChange({ [settingKey]: e.target.checked })}
+      />
+    </FormControl>
+  );
+}
+
+function SectionHeading({ children }) {
+  return (
+    <Heading size="xs" textTransform="uppercase" letterSpacing="wide" mb="2" mt="4" opacity="0.6">
+      {children}
+    </Heading>
+  );
+}
 
 export default function SettingsModal({ isOpen, onClose }) {
   const [APIKey, saveAPIKey] = useAtom(APIKeyAtom);
@@ -91,9 +117,13 @@ export default function SettingsModal({ isOpen, onClose }) {
     });
   };
 
+  const s = settingsState.appSettings;
+
   return (
     <Modal
       isOpen={isOpen}
+      size="xl"
+      scrollBehavior="inside"
       onClose={() => {
         if (APIKey !== "") {
           onClose();
@@ -104,261 +134,225 @@ export default function SettingsModal({ isOpen, onClose }) {
       <ModalContent>
         <ModalHeader>Settings</ModalHeader>
         {APIKey !== "" && <ModalCloseButton />}
-        <ModalBody>
-          <FormControl>
-            <FormLabel>Zenhub API key</FormLabel>
-            <Input
-              placeholder="API Key"
-              value={settingsState.APIKey}
-              onChange={(e) => {
-                updateSettings({ APIKey: e.target.value });
-              }}
-            />
-            <FormHelperText>
-              To generate your Personal API Key, go to the{" "}
-              <Link
-                href="https://app.zenhub.com/settings/tokens"
-                isExternal
-                color="teal.500"
-              >
-                API section of your Zenhub Dashboard
-              </Link>
-              .
-            </FormHelperText>
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Theme (takes effect immediately)</FormLabel>
-            <RadioGroup
-              value={colorModePreference}
-              onChange={setColorModePreference}
-            >
-              <HStack spacing="24px">
-                <Radio value="light">Light</Radio>
-                <Radio value="dark">Dark</Radio>
-                <Radio value="system">System</Radio>
-              </HStack>
-            </RadioGroup>
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Snap to grid</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.snapToGrid}
-              onChange={(e) => {
-                updateAppSettings({
-                  snapToGrid: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show issue details</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showIssueDetails}
-              onChange={(e) => {
-                updateAppSettings({
-                  showIssueDetails: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show non-epic issues</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showNonEpicIssues}
-              onChange={(e) => {
-                updateAppSettings({
-                  showNonEpicIssues: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show self-contained issues</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showSelfContainedIssues}
-              onChange={(e) => {
-                updateAppSettings({
-                  showSelfContainedIssues: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show closed epics</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showClosedEpics}
-              onChange={(e) => {
-                updateAppSettings({
-                  showClosedEpics: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show issue estimates</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showIssueEstimates}
-              onChange={(e) => {
-                updateAppSettings({
-                  showIssueEstimates: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show issue sprints</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showIssueSprints}
-              onChange={(e) => {
-                updateAppSettings({
-                  showIssueSprints: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Show issue previews</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.showIssuePreviews}
-              onChange={(e) => {
-                updateAppSettings({
-                  showIssuePreviews: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Highlight blocked & blocking issues</FormLabel>
-            <Switch
-              isChecked={settingsState.appSettings.highlightRelatedIssues}
-              onChange={(e) => {
-                updateAppSettings({
-                  highlightRelatedIssues: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          <FormControl pt="5">
-            <FormLabel>Include background when exporting graph</FormLabel>
-            <Switch
-              isChecked={
-                settingsState.appSettings.includeBackgroundWhenExporting
-              }
-              onChange={(e) => {
-                updateAppSettings({
-                  includeBackgroundWhenExporting: e.target.checked,
-                });
-              }}
-            />
-          </FormControl>
-          {Object.keys(coordinateOverrides || {}).length > 0 && (
-            <FormControl pt="5">
-              <FormLabel>
-                Reset epic layout (takes effect immediately)
-              </FormLabel>
-              <Button
-                colorScheme="blue"
-                mr={1}
-                onClick={() => {
-                  saveCoordinateOverrides(null);
-                }}
-              >
-                Reset layout
-              </Button>
-            </FormControl>
-          )}
-          {!(
-            deepEquals(pipelineColors, pipelineColorDefaults) &&
-            deepEquals(additionalColors, additionalColorDefaults)
-          ) && (
-            <FormControl pt="5">
-              <FormLabel>
-                Reset issue colours (takes effect immediately)
-              </FormLabel>
-              <Button
-                colorScheme="blue"
-                mr={1}
-                onClick={() => {
-                  savePipelineColors(pipelineColorDefaults);
-                  saveAdditionalColors(additionalColorDefaults);
-                }}
-              >
-                Reset colours
-              </Button>
-            </FormControl>
-          )}
-          {Object.keys(pipelineHidden).length > 0 && (
-            <FormControl pt="5">
-              <FormLabel>
-                Unhide all pipelines (takes effect immediately)
-              </FormLabel>
-              <Button
-                colorScheme="blue"
-                mr={1}
-                onClick={() => {
-                  savePipelineHidden({});
-                }}
-              >
-                Unhide pipelines
-              </Button>
-            </FormControl>
-          )}
-          <FormControl pt="5" pb="5">
-            <FormLabel>
-              Flush request cache (takes effect immediately)
-            </FormLabel>
-            <Button
-              colorScheme="blue"
-              mr={1}
-              onClick={() => {
-                clearGraphCache();
-              }}
-            >
-              Flush cache
-            </Button>
-          </FormControl>
-          <details>
-            <summary>Advanced</summary>
+        <ModalBody px="0" pb="0">
+          <Tabs colorScheme="blue" isLazy>
+            <TabList px="6">
+              <Tab>General</Tab>
+              <Tab>Display</Tab>
+              <Tab>Canvas &amp; Export</Tab>
+              <Tab>Maintenance</Tab>
+              <Tab>Advanced</Tab>
+            </TabList>
 
-            <FormControl pt="5">
-              <FormLabel>Show grid</FormLabel>
-              <Switch
-                isChecked={settingsState.appSettings.showGrid}
-                onChange={(e) => {
-                  updateAppSettings({
-                    showGrid: e.target.checked,
-                  });
-                }}
-              />
-            </FormControl>
-            <FormControl pt="5">
-              <FormLabel>
-                Show non-epic blocked issues (it's recommended to leave this
-                off)
-              </FormLabel>
-              <Switch
-                isChecked={settingsState.appSettings.showNonEpicBlockedIssues}
-                onChange={(e) => {
-                  updateAppSettings({
-                    showNonEpicBlockedIssues: e.target.checked,
-                  });
-                }}
-              />
-            </FormControl>
-            <FormControl pt="5">
-              <FormLabel>
-                Show ancestor dependencies (it's recommended to leave this off)
-              </FormLabel>
-              <Switch
-                isChecked={settingsState.appSettings.showAncestorDependencies}
-                onChange={(e) => {
-                  updateAppSettings({
-                    showAncestorDependencies: e.target.checked,
-                  });
-                }}
-              />
-            </FormControl>
-          </details>
+            <TabPanels>
+              {/* General */}
+              <TabPanel>
+                <SectionHeading>Connection</SectionHeading>
+                <FormControl>
+                  <FormLabel>Zenhub API key</FormLabel>
+                  <Input
+                    placeholder="API Key"
+                    value={settingsState.APIKey}
+                    onChange={(e) => {
+                      updateSettings({ APIKey: e.target.value });
+                    }}
+                  />
+                  <FormHelperText>
+                    To generate your Personal API Key, go to the{" "}
+                    <Link
+                      href="https://app.zenhub.com/settings/tokens"
+                      isExternal
+                      color="teal.500"
+                    >
+                      API section of your Zenhub Dashboard
+                    </Link>
+                    .
+                  </FormHelperText>
+                </FormControl>
+
+                <SectionHeading>Appearance</SectionHeading>
+                <FormControl pt="2">
+                  <FormLabel>Theme (takes effect immediately)</FormLabel>
+                  <RadioGroup
+                    value={colorModePreference}
+                    onChange={setColorModePreference}
+                  >
+                    <HStack spacing="24px">
+                      <Radio value="light">Light</Radio>
+                      <Radio value="dark">Dark</Radio>
+                      <Radio value="system">System</Radio>
+                    </HStack>
+                  </RadioGroup>
+                </FormControl>
+              </TabPanel>
+
+              {/* Display */}
+              <TabPanel>
+                <SectionHeading>Graph contents</SectionHeading>
+                <SwitchSetting
+                  label="Show non-epic issues"
+                  settingKey="showNonEpicIssues"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show self-contained issues"
+                  settingKey="showSelfContainedIssues"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show closed epics"
+                  settingKey="showClosedEpics"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+
+                <SectionHeading>Issue card details</SectionHeading>
+                <SwitchSetting
+                  label="Show issue details"
+                  settingKey="showIssueDetails"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show issue estimates"
+                  settingKey="showIssueEstimates"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show issue sprints"
+                  settingKey="showIssueSprints"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show issue previews"
+                  settingKey="showIssuePreviews"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+              </TabPanel>
+
+              {/* Canvas & Export */}
+              <TabPanel>
+                <SectionHeading>Canvas &amp; interaction</SectionHeading>
+                <SwitchSetting
+                  label="Snap to grid"
+                  settingKey="snapToGrid"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Highlight blocked &amp; blocking issues"
+                  settingKey="highlightRelatedIssues"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+
+                <SectionHeading>Export</SectionHeading>
+                <SwitchSetting
+                  label="Include background when exporting graph"
+                  settingKey="includeBackgroundWhenExporting"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+              </TabPanel>
+
+              {/* Maintenance */}
+              <TabPanel>
+                {Object.keys(coordinateOverrides || {}).length > 0 && (
+                  <FormControl pt="4">
+                    <FormLabel>
+                      Reset epic layout (takes effect immediately)
+                    </FormLabel>
+                    <Button
+                      colorScheme="blue"
+                      mr={1}
+                      onClick={() => {
+                        saveCoordinateOverrides(null);
+                      }}
+                    >
+                      Reset layout
+                    </Button>
+                  </FormControl>
+                )}
+                {!(
+                  deepEquals(pipelineColors, pipelineColorDefaults) &&
+                  deepEquals(additionalColors, additionalColorDefaults)
+                ) && (
+                  <FormControl pt="4">
+                    <FormLabel>
+                      Reset issue colours (takes effect immediately)
+                    </FormLabel>
+                    <Button
+                      colorScheme="blue"
+                      mr={1}
+                      onClick={() => {
+                        savePipelineColors(pipelineColorDefaults);
+                        saveAdditionalColors(additionalColorDefaults);
+                      }}
+                    >
+                      Reset colours
+                    </Button>
+                  </FormControl>
+                )}
+                {Object.keys(pipelineHidden).length > 0 && (
+                  <FormControl pt="4">
+                    <FormLabel>
+                      Unhide all pipelines (takes effect immediately)
+                    </FormLabel>
+                    <Button
+                      colorScheme="blue"
+                      mr={1}
+                      onClick={() => {
+                        savePipelineHidden({});
+                      }}
+                    >
+                      Unhide pipelines
+                    </Button>
+                  </FormControl>
+                )}
+                <FormControl pt="4" pb="4">
+                  <FormLabel>
+                    Flush request cache (takes effect immediately)
+                  </FormLabel>
+                  <Button
+                    colorScheme="blue"
+                    mr={1}
+                    onClick={() => {
+                      clearGraphCache();
+                    }}
+                  >
+                    Flush cache
+                  </Button>
+                </FormControl>
+              </TabPanel>
+
+              {/* Advanced */}
+              <TabPanel>
+                <SwitchSetting
+                  label="Show grid"
+                  settingKey="showGrid"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show non-epic blocked issues (it's recommended to leave this off)"
+                  settingKey="showNonEpicBlockedIssues"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+                <SwitchSetting
+                  label="Show ancestor dependencies (it's recommended to leave this off)"
+                  settingKey="showAncestorDependencies"
+                  settings={s}
+                  onChange={updateAppSettings}
+                />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </ModalBody>
 
         <ModalFooter>
